43ca602b9a002f4ef24ce212378d55972063380f
Maurits Meester <maurits@e-mmer.nl>
2025-02-19 20:56:00 +0100

    feat(header): first steps into a new header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,113 @@
 <template>
-  <div>
-    <nav>
-      <NuxtImg src="/images/het-broeker-huis-logo.svg" class="topnav__logo" />
-    </nav>
-    <slot />
-    footer
+  <div class="layout">
+    <header class="header">
+      <nav class="topnav">
+        <NuxtImg src="/images/het-broeker-huis-logo.svg" class="topnav__logo" />
+      </nav>
+      <div class="header__wordmark">
+        <NuxtImg
+          src="/images/het-broeker-huis-wordmark.svg"
+          class="header__wordmark-image"
+        />
+      </div>
+
+      <div class="header__background">
+        <NuxtImg src="/images/IMG_5047.jpeg" class="header__background-image" />
+        <NuxtImg
+          src="/images/header-overlay.svg"
+          class="header__background-overlay"
+        />
+      </div>
+    </header>
+    <main class="main">
+      <slot />
+    </main>
+    <footer class="footer">Footer</footer>
   </div>
 </template>
 
 <style>
+.layout {
+  display: grid;
+  grid-template-columns: repeat(26, 1fr);
+}
+
+.header,
+.main,
+.footer {
+  display: grid;
+  grid-column: 1 / -1;
+  grid-template-columns: subgrid;
+}
+
+.header {
+  position: relative;
+  display: grid;
+  grid-template-rows: fit-content(100%) auto;
+  padding: 1rem 0;
+  background: #2e322d;
+  aspect-ratio: 9 / 5;
+
+  &__wordmark {
+    position: relative;
+    z-index: 1;
+    display: block;
+    grid-column: 9 / span 9;
+    align-self: center;
+
+    &-image {
+      width: 100%;
+    }
+  }
+
+  &__background {
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 0;
+    width: 50vw;
+    height: 100%;
+
+    &::before {
+      position: absolute;
+      top: 0;
+      right: 0;
+      left: 0;
+      height: 30%;
+      background: linear-gradient(
+        180deg,
+        rgb(0 0 0 / 60%) 0%,
+        rgb(0 0 0 / 0%) 100%
+      );
+      content: "";
+    }
+
+    &-image {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    &-overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 1;
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+
 .topnav {
+  position: relative;
+  z-index: 1;
+  display: grid;
+  grid-column: 1 / -1;
+
   &__logo {
     display: block;
+    width: 3.33vw;
     margin: 0 auto;
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
-const { data: home } = await useAsyncData(() => queryCollection('content').path('/').first())
+const { data: home } = await useAsyncData(() =>
+  queryCollection("content").path("/").first(),
+);
 
 useSeoMeta({
   title: home.value?.title,
-  description: home.value?.description
-})
+  description: home.value?.description,
+});
 </script>
 
 <template>
-  <ContentRenderer v-if="home" :value="home" />
-  <div v-else>Home not found</div>
+  <div class="content">
+    <ContentRenderer v-if="home" :value="home" />
+    <div v-else>Home not found</div>
+  </div>
 </template>
+
+<style>
+.content {
+  grid-column: 9 / span 9;
+  margin-top: 10.5rem;
+  text-align: center;
+}
+</style>
