724d2efefec8213cc3ee442c9022cffcd8c63e61
Maurits Meester <maurits@e-mmer.nl>
2025-02-19 16:46:34 +0100

    feat(layout): add some first layout things

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nav>
+      <NuxtImg src="/images/het-broeker-huis-logo.svg" class="topnav__logo" />
+    </nav>
+    <slot />
+    footer
+  </div>
+</template>
+
+<style>
+.topnav {
+  &__logo {
+    display: block;
+    margin: 0 auto;
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/content"],
+  modules: ["@nuxt/eslint", "@nuxt/content", "@nuxt/image"],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       "postcss-nested": {},
